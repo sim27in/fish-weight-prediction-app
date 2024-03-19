fb9fc04daa02599c6d7a5255af4456cf8b0925f8
sim27in <sim27in@gmail.com>
2024-03-18 21:21:50 -0400

    cleaned code

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-# data split and transformation
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
@@ -81,7 +80,7 @@ fish_data[["Length1", "Length2", "Length3", "Weight"]].corr()
 fish_data[["Length1", "Length2", "Length3"]].head(10)
 
 
-
+#Length1 and 2 have more correlation so dropping one
 fish_data = fish_data.drop(["Length1"], axis=1)
 
 # Selecting features and target variable
@@ -122,7 +121,7 @@ print(f"Random Forest Test MSE: {mse}")
 print(f"Random Forest Test R^2 Score: {r2}")
 
 
-# Ensure all necessary imports are present
+# saving model in pkl
 pickle.dump(pipeline, open('model.pkl','wb'))
 
 
